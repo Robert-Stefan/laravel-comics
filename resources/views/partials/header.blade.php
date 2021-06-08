96fc596895aa1dfd4b7d7a0c0af3aee4b833e8ef
Robert-Stefan <robertboss05@hotmail.it>
2021-06-08 17:27:55 +0200

    Add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="container">
+        <a href="/">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
+        </a>
+
+        <ul>
+            <li>
+                <a class="active" href="/">Comics</a>
+            </li>
+            <li>
+                <a href="/news">News</a>
+            </li>
+        </ul>
+    </div>
+</header>
